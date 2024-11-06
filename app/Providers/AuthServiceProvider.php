d63947a117d05faabd08fa18016e878f7d7e661d
lion-jpg <nikorobert108@gmail.com>
2024-11-06 17:14:41 +0100

    provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -10,20 +10,14 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
+    
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         User::class => UserPolicy::class,
         'App\Filament\Pages\Images' => ImageViewerPolicy::class,
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
+    
     public function boot(): void
     {
         $this->registerPolicies();
