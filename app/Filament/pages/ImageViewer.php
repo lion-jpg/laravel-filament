336109f869f7e07ae3711af79808ca3ce591b956
lion-jpg <nikorobert108@gmail.com>
2024-11-06 17:00:10 +0100

    imagenViewer

--- app/Filament/pages/ImageViewer.php
+++ app/Filament/pages/ImageViewer.php
@@ -9,15 +9,26 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class ImageViewer extends Page
 {
     //direccion a link guia
-    protected static string $view = 'filament.pages.Guias-Registrados';
+    protected static string $view = 'filament.pages.Image-Viewer';
     protected static ?string $navigationLabel = 'Guias Registrados'; // Cambia el nombre que aparecerá en la navegación
     protected static ?string $navigationIcon = 'heroicon-o-user-plus';
     
     public function mount()
     {   
-        if (!auth()->user() || !policy(static::class)->view(auth()->user())) {
-            abort(403); // Acceso denegado
+        // Verifica si el usuario está autenticado
+        if (!auth()->check()) {
+            // dd(auth()->user()) ;
+            return redirect()->route('login'); // Redirige a la página de inicio de sesión
         }
+        
+        $user = auth()->user();
+        // dd($user->roles->pluck('name'));
+
+         
+    
+        // if (!ImageViwerPolicy(static::class)->view(auth()->user())) {
+        //     abort(403); // Acceso denegado
+        // }
         return redirect('admin/guia');
-    }
-}
+    
+}}
